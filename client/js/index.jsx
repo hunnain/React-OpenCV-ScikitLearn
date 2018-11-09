5689357b8818f5ad0728468db523e4067b6f508d
hunnain <hunnainpashahgchgc@gmail.com>
2018-11-09 20:42:35 +0500

    Client Index

---
+++ client/js/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/home/app";
+import Routers from './routers/router';
+ReactDOM.render(<Routers />, document.getElementById("content"));
