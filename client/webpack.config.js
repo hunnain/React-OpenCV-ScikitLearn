0b310cd132443af292b6bb25f0f630799f54fbc1
hunnain <hunnainpashahgchgc@gmail.com>
2018-11-09 20:43:52 +0500

    Webpack config

---
+++ client/webpack.config.js
@@ -0,0 +1,40 @@
+const config = {
+    entry:  __dirname + '/js/index.jsx',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']
+    },
+    devServer: {
+        inline: true,
+        contentBase: './',
+        port: 3001
+    },
+    watch: true,
+    module: {
+        rules: [
+          {
+            test: /\.jsx?/,
+            exclude: /node_modules/,
+            use: 'babel-loader'
+          },
+
+            {
+                test: /\.css?$/,
+                loaders: [ 'style-loader', 'css-loader' ],
+            },
+            {
+                test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+                loader : 'file-loader'
+            }
+          ],
+
+          //plugins
+          
+      }
+
+
+};
+module.exports = config;
